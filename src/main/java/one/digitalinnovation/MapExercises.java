0a36a93cefbd7c7530a7f818426fdcf058cd0467
Roberto R Jr <rrjr87@outlook.com>
2021-07-12 18:16:58 -0300

    working on map exercises

--- src/main/java/one/digitalinnovation/MapExercises.java
+++ src/main/java/one/digitalinnovation/MapExercises.java
@@ -1,7 +1,6 @@
 package one.digitalinnovation;
 
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
 
 public class MapExercises {
@@ -9,16 +8,42 @@ public class MapExercises {
         System.out.println("Crie um Map e execute as seguintes operações:\n");
         Map<String, String> statesMap = new HashMap<>();
 
-        System.out.println("1- Adicione os 26 estados Brasileiros no map, " +
+        System.out.println("1- Adicione os 26 Estados Brasileiros no map, " +
                             "onde a sigla será a chave e o nome do estado o valor");
-
-        System.out.println(statesMap);
+            statesMap.put("AC", "Acre");
+            statesMap.put("AL", "Alagoas");
+            statesMap.put("AP", "Amapa");
+            statesMap.put("AM", "Amazonas");
+            statesMap.put("BA", "Bahia");
+            statesMap.put("CE", "Ceara");
+            statesMap.put("ES", "Espirito Santo");
+            statesMap.put("GO", "Goias");
+            statesMap.put("MA", "Maranhao");
+            statesMap.put("MT", "Mato Grosso");
+            statesMap.put("MS", "Mato Grosso do Sul");
+            statesMap.put("MG", "Minas Gerais");
+            statesMap.put("PA", "Para");
+            statesMap.put("PB", "Paraiba");
+            statesMap.put("PR", "Parana");
+            statesMap.put("PE", "Pernambuco");
+            statesMap.put("PI", "Piaui");
+            statesMap.put("RJ", "Rio de Janeiro");
+            statesMap.put("RN", "Rio Grande do Norte");
+            statesMap.put("RO", "Rondonia");
+            statesMap.put("RR", "Roraima");
+            statesMap.put("SC", "Santa Catarina");
+            statesMap.put("SP", "São Paulo");
+            statesMap.put("SE", "Sergipe");
+            statesMap.put("TO", "Tocantins");
+            System.out.println(statesMap);
 
         System.out.println("\n2- Remova o estado de Minas Gerais");
-
+            statesMap.remove("MG");
+            System.out.println(statesMap);
 
         System.out.println("\n3- Adicione o Distrito Federal");
-
+            statesMap.put("DF", "Distrito Federal");
+            System.out.println(statesMap);
         System.out.println("\n4- Verifique o tamanho do mapa");
 
         System.out.println("\n5- Remova o estado do Mato Grosso do Sul usando o nome");
