6fba3b46f06176fa0bd2075a7119320ab40e06cb
Roberto R Jr <rrjr87@outlook.com>
2021-07-13 16:33:28 -0300

    added Optional exercises

--- src/main/java/one/digitalinnovation/OptionalExercises.java
+++ src/main/java/one/digitalinnovation/OptionalExercises.java
@@ -1,28 +1,35 @@
 package one.digitalinnovation;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Optional;
 
 public class OptionalExercises {
     public static void main(String[] args) {
         System.out.println("Crie um optional de um determinado tipo de dado:\n");
 
         System.out.println("1- Crie com estado vazio, estado presente e com null\n");
+        Optional<String> presentOptional = Optional.of("Presente");
+        Optional<String> emptyOptional = Optional.empty();
+        Optional<String> nullOptional = Optional.ofNullable(null);
 
+        System.out.println(presentOptional.get());
+        System.out.println(emptyOptional);
+        System.out.println(nullOptional + "\n");
 
-        System.out.println("2- Se vazio, lance uma exceção IlegalStateExpresion\n");
+        System.out.println("2- Se vazio, exiba 'Optional vazio' no console\n");
+        if (emptyOptional.isEmpty()) {
+            System.out.println("Optional vazio\n");
+        }
 
-        System.out.println("3- Se vazio, exiba 'Optional vazio' no console\n");
+        System.out.println("3- Se presente, transforme o valor e exiba no console\n");
+        if (presentOptional.isPresent()) {
+            System.out.println(presentOptional.of("Alterado").get());
+        }
 
+        System.out.println("4- Se presente, pegue o valor do Optional e atribua em uma variável\n");
+        if (presentOptional.isPresent()) {
+            String value = presentOptional.get();
+            System.out.println(value);
+        }
 
-        System.out.println("4- Se presente, transforme o valor e exiba no console\n");
-
-
-        System.out.println("5- Se presente, pegue o valor do Optional e atribua em uma variável\n");
-
-
-        System.out.println("6- Se presente, filtre o optional com uma determinada regra de negócios\n");
     }
-
-
 }
