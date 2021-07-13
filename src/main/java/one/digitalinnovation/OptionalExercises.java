42a561f4b5963a16b7ea203664075c1700595159
Roberto R Jr <rrjr87@outlook.com>
2021-07-13 15:42:35 -0300

    working on Optional exercises

---
+++ src/main/java/one/digitalinnovation/OptionalExercises.java
@@ -0,0 +1,28 @@
+package one.digitalinnovation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OptionalExercises {
+    public static void main(String[] args) {
+        System.out.println("Crie um optional de um determinado tipo de dado:\n");
+
+        System.out.println("1- Crie com estado vazio, estado presente e com null\n");
+
+
+        System.out.println("2- Se vazio, lance uma exceção IlegalStateExpresion\n");
+
+        System.out.println("3- Se vazio, exiba 'Optional vazio' no console\n");
+
+
+        System.out.println("4- Se presente, transforme o valor e exiba no console\n");
+
+
+        System.out.println("5- Se presente, pegue o valor do Optional e atribua em uma variável\n");
+
+
+        System.out.println("6- Se presente, filtre o optional com uma determinada regra de negócios\n");
+    }
+
+
+}
